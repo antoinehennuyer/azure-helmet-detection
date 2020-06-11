878fcae2ba39520b10d0d36a2d4c9ca11868ba34
antoinehennuyer <47240189+antoinehennuyer@users.noreply.github.com>
2020-06-11 14:57:00 +0200

    Update helmet-prediction.py

--- helmet-prediction.py
+++ helmet-prediction.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt, patches
+from matplotlib import image
 
 path = "test_img.jpg"
 
@@ -657,12 +658,19 @@ json_test = {
 }
 img = plt.imread(path)
 figure, ax = plt.subplots(1)
-rect = patches.Rectangle((125,100),50,25, edgecolor='r', facecolor="none")
+
 prediction_list = json_test["predictions"]
 for predic in prediction_list:
     if predic["probability"] > 0.5:
         print("prediction detected ! It is:" + predic["tagName"])
-
+        left_corner = predic["boundingBox"]["left"] * img.shape[1]
+        top_corner = predic["boundingBox"]["top"] * img.shape[0]
+        width = predic["boundingBox"]["width"] * img.shape[1]
+        height = predic["boundingBox"]["height"] * img.shape[0]
+        rect = patches.Rectangle((left_corner, top_corner), width, height, edgecolor='r', facecolor="none")
+        ax.add_patch(rect)
+        ax.imshow(img)
+        plt.show()
 
 # PRINT IF SAS OPEN OR NOT
 
